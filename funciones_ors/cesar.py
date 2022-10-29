0fcd53e5e65d7335909fd10785410df180f7a49e
Rober <rp_rober@hotmail.com>
2022-10-29 20:55:48 +0200

    curso

--- funciones_ors/cesar.py
+++ funciones_ors/cesar.py
@@ -3,4 +3,14 @@ ALFABETO = list("ABCDEFGHIJKLMNÑOPQRSTUVWXYZ")
 def cesar(caracter, clave):
     posicion =ALFABETO.index(caracter)
     nuevaposicion = posicion + clave
+    nuevaposicion = nuevaposicion % len(ALFABETO)
     return ALFABETO[nuevaposicion]
+
+def cifrar(mensaje, clave):
+    resultado = ""
+    for caracter in mensaje:
+        nuevo_caracter = cesar(caracter, clave)
+        resultado += nuevo_caracter
+        #resultado = resultado + nuevo_caracter
+    return resultado
+
--- funciones_ors/main_cesar.py
+++ funciones_ors/main_cesar.py
@@ -1,7 +1,12 @@
-from cesar import cesar
+from cesar import cesar,cifrar
 
 print(cesar("A", 1)) #B
 
 print(cesar("C", 3)) #F
 
-print(cesar("Z", 1)) #
+print(cesar("Z", 1)) #A
+
+print(cifrar("ZAR", 1))#ABS
+
+#descifrar seria lo mismo en clave -1
+print(cifrar("ABS", -1))
